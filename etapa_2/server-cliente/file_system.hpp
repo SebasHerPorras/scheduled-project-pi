aa91b07a776ca7d4ef02ca4dbc163542ef3c91c0
fxbrc <agueromatafabricio@gmail.com>
2025-04-30 21:40:20 -0600

    No se hicieron cambios

--- etapa_2/server-cliente/file_system.hpp
+++ etapa_2/server-cliente/file_system.hpp
@@ -22,8 +22,7 @@ class FileSystem {
     FileSystem();
     FileSystem(bool);
     ~FileSystem();
-    void open_file(const std::string& filePath);
-    void open_file();
+    
     void close_file();
     void escribir_bloque(int bloque_num, const char* datos);
     void leer_root();
@@ -31,14 +30,15 @@ class FileSystem {
     void formatear_sistema();
     void init_file();
     void guardar_archivo( char* nombre_archivo, const char* datos);
-    int find_free_block(); // Método para encontrar un bloque libre en el sistema de archivos
     void poner_ceros();
     void cargar_figuras_base();
+    int find_free_block(); // Método para encontrar un bloque libre en el sistema de archivos
     short find_figura_post(const std::string& nombre_figura);
     char* find_figura(const std::string& nombre_figura);
     char* find_figura_char(short bloque);
     char* find_figura_chara(short bloque);
     char* find_figura_error();
+    std::vector<std::string> get_figuras();
   private:
     std::string filePath;
     std::fstream archivo;
